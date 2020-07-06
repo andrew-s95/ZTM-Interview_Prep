9a15a3835bf6723749d6394999beab6eac864431
andrew-s95 <andrew.s95@outlook.com>
2020-07-06 10:26:54 -0700

    Factorial/Memoization

---
+++ factorial.py
@@ -0,0 +1,13 @@
+#memoization
+def factorial(nums):
+    if num == 1:
+        return 1
+    return num * factorial(num-1)
+
+#iterative
+def factorial2(nums):
+    result = 1
+    for i in range(1, nums+1):
+        result *= if
+    return result
+    
